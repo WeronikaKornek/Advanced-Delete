6156f39f19d2aaf059900fb14487f12029f086e8
WeronikaKornek <117509329+WeronikaKornek@users.noreply.github.com>
2022-12-11 18:48:54 +0100

    remove product

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ProductModel } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-product-list',
+  styleUrls: ['./product-list.component.scss'],
+  templateUrl: './product-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  readonly list$: Observable<ProductModel[]> = this._productsService.getAll();
+  private _refreshSubject: BehaviorSubject<void> = new BehaviorSubject<void>(void 0);
+  public refresh$: Observable<void> = this._refreshSubject.asObservable();
+  readonly refreshList$: Observable<ProductModel[]> = this.refresh$.pipe(
+    switchMap(data => this._productsService.getAll())
+  );
+
+  constructor(private _productsService: ProductsService) {
+  }
+
+  delete(id: number): void {
+    this._productsService.delete(String(id)).subscribe(() => this._refreshSubject.next());
+  }
+}
